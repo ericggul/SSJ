69fefe11de9fe50bd677f756f9ba43f6f08bdb62
ericggul <57379903+ericggul@users.noreply.github.com>
2021-03-07 17:13:37 +0900

    디자인중

---
+++ main.js
@@ -0,0 +1,13 @@
+function validateform(){
+	var name = document.forms["#register"]["#name"].value;
+	var tel = document.forms["#register"]["#tel"].value;
+
+	if (name==null||name=""){
+	alert("Name is null")
+	return false;
+	} else if (tel==null||tel=""){
+		alert("Telephone is null")
+		return false;
+	}
+
+}
