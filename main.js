b6fa47778e6d53e4e9e078b42af1d6ba8d44b6ac
ericggul <57379903+ericggul@users.noreply.github.com>
2021-03-10 10:30:58 +0900

    Test

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ function validateform(){
 	var name = document.forms["#register"]["#name"].value;
 	var tel = document.forms["#register"]["#tel"].value;
 
-	if (name==null||name=""){
+	if (name==null||name=" "){
 	alert("Name is null")
 	return false;
 	} else if (tel==null||tel=""){
-		alert("Telephone is null")
+		alert("Telephone is null");
 		return false;
 	}
 
